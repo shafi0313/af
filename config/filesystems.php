0061b97ffda0bf9b8d00474837004126691282f0
shafi97 <msh.shafiul@gmail.com>
2023-02-25 16:44:26 +0600

    b

--- config/filesystems.php
+++ config/filesystems.php
@@ -46,6 +46,7 @@ return [
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app'),
+            'throw' => false,
         ],
 
         'public' => [
@@ -53,6 +54,7 @@ return [
             'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
+            'throw' => false,
         ],
 
         's3' => [
